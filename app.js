de4eb01a334c4f558a4c8a6a702b726426e7851a
dejanV91 <dejanvukoicic91@gmail.com>
2022-07-21 18:09:00 +0200

    initial commit

--- app.js
+++ app.js
@@ -3,10 +3,38 @@ const next = document.getElementById("next");
 const prev = document.getElementById("previous");
 
 let counter = 0;
+const numb = singleImg.childElementCount;
 
-next.addEventListener("click", function(){
-    if (counter == 0) {
-        
-    }
-    singleImg.style.transform = `translateX(-${counter * 100}%)`
-});
+
+    next.addEventListener("click", function(){
+        if(counter > 0 && counter < numb-2){
+            counter++;
+            singleImg.style.transform = `translateX(-${counter * 100}%)`
+        }
+        else if (counter == 0) {
+            counter++
+            singleImg.style.transform = `translateX(-${counter * 100}%)`
+            prev.classList.remove("hidden");
+        }else{
+            counter++;
+            singleImg.style.transform = `translateX(-${counter * 100}%)`
+            next.classList.add("hidden");
+        }
+    });
+
+    prev.addEventListener("click", function(){
+        if(counter > 2 && counter < numb-1){
+            counter--;
+            singleImg.style.transform = `translateX(-${counter * 100}%)`
+        }
+        else if (counter == 1) {
+            counter--
+            singleImg.style.transform = `translateX(-${counter * 100}%)`
+            prev.classList.add("hidden");
+        }else{
+            counter--;
+            singleImg.style.transform = `translateX(-${counter * 100}%)`
+            next.classList.remove("hidden");
+        }
+    });
+
